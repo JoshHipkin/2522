0669a3f3260567b8ac28aee4d833dd7c8c9b6136
Josh Hipkin <j.hipkin4@gmail.com>
2023-10-02 12:31:52 -0700

    uploaded A1c

---
+++ A1c/src/BadWidthException.java
@@ -0,0 +1,9 @@
+import java.lang.Exception;
+
+/**
+ * This exception is thrown when a non-odd width is used to create either
+ * a triangle or diamond.
+ */
+public class BadWidthException extends Exception{
+
+}
---
+++ A1c/src/Rectangle.java
@@ -0,0 +1,23 @@
+/**
+ * Represents a rectangle, which is a specific type of geometric shape, extending the base Shape class.
+ * It initializes and populates a 2D grid to represent the rectangle with '*' characters.
+ */
+public class Rectangle extends Shape {
+
+  /**
+   * Constructs a new Rectangle object with the specified width and height.
+   *
+   * @param w The width of the rectangle.
+   * @param h The height of the rectangle.
+   */
+  protected Rectangle(int w, int h) {
+    super(w, h, "r");
+
+    // Fill the array with '*' to represent a rectangle.
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        grid[i][j] = "*";
+      }
+    }
+  }
+}
